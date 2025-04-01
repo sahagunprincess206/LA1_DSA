1099200e466b3d1102e939c822892bb2aee46112
sahagunprincess206 <sahagunprincess206@gmail.com>
2025-04-01 14:48:51 +0800

    Add files via upload

---
+++ LA1_SAHAGUN_CS1A/Variables.js
@@ -0,0 +1,7 @@
+//let the user input their name
+let name = "Princess Sahagun"
+let whatDoYouWannaBecomeInYourLife = "Software Engineering";
+let gender = " Female";
+let twitterHandle = " @ez_issay";
+// console ti see the output
+console.log(name + "\n" + whatDoYouWannaBecomeInYourLife + "\n" + gender + "\n" + twitterHandle)
