5cdb2204561c8c8ac17258eeaa8b354af3e257d0
sahagunprincess206 <sahagunprincess206@gmail.com>
2025-04-01 14:49:33 +0800

    Add files via upload

---
+++ LA1_SAHAGUN_CS1A/strings.js
@@ -0,0 +1,16 @@
+//let Define the favActor Name
+let favActorFirstName = "Jackie";
+let favActorLastName = "Chan";
+
+ 
+let fullName = favActorFirstName + " " + favActorLastName;
+let uppercase = fullName.toUpperCase();
+let message = "My favorite actor is " + uppercase;
+message += " you are the best and one of my favMovie is karate kid";
+
+//Console the results of actor name
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(message);
