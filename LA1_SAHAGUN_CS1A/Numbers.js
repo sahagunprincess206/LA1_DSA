1f8d2db87c1b5060474c126927cae29ca67d3d65
sahagunprincess206 <sahagunprincess206@gmail.com>
2025-04-01 14:50:52 +0800

    Add files via upload

---
+++ LA1_SAHAGUN_CS1A/Numbers.js
@@ -0,0 +1,14 @@
+// let my fav numbers
+let firstFavNum = 30
+let secondFavNum = 19
+
+// by performing of different operations
+let addition = firstFavNum + secondFavNum;
+let subtraction = firstFavNum - secondFavNum;
+let multiplication = firstFavNum * secondFavNum;
+let division = firstFavNum / secondFavNum;
+let modulos = firstFavNum % secondFavNum;
+let exponentation = firstFavNum ** secondFavNum;
+
+// console the output
+console.log("Addition: " + addition + "\n" + "Subtraction: " + subtraction + "\n" + "Multiplication: " + multiplication + "\n" + "Division:" + division + "\n" + "Modulos: " + modulos + "\n" + "Exponentation(Power): " + exponentation)
