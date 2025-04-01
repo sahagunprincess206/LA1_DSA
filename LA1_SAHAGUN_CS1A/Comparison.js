ae30f00fd3ca03b25002bbac781e04e1d1ce7f7c
sahagunprincess206 <sahagunprincess206@gmail.com>
2025-04-01 14:50:26 +0800

    Add files via upload

---
+++ LA1_SAHAGUN_CS1A/Comparison.js
@@ -0,0 +1,22 @@
+//let the FavNumbers
+let firstFavNumb = 30;
+let secondFavNumb = 19;
+
+let greater_than = firstFavNumb > secondFavNumb;
+let less_than = firstFavNumb < secondFavNumb;
+let greaterThan_equal = firstFavNumb >= secondFavNumb;
+let lessThan_equal = firstFavNumb <= secondFavNumb;
+let strict_equality = firstFavNumb === secondFavNumb;
+let loose_equality = firstFavNumb == secondFavNumb;
+let strict_inequality = firstFavNumb !== secondFavNumb;
+let loose_inequality = firstFavNumb != secondFavNumb;
+
+// console to Display the results  
+console.log("Greater Than: ", greater_than);
+console.log("Less Than: ", less_than);
+console.log("Greater Than or Equal to: ", greaterThan_equal);
+console.log("Less Than or Equal to: ", lessThan_equal);
+console.log("Strict Equality: ", strict_equality);
+console.log("Loose Equality: ", loose_equality);
+console.log("Strict Inequality: ", strict_inequality);
+console.log("Loose Inequality: ", loose_inequality);
