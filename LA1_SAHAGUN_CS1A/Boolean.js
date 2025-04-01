24177dabcf592c4f3503a25c3c8aa2261efff894
sahagunprincess206 <sahagunprincess206@gmail.com>
2025-04-01 14:50:06 +0800

    Add files via upload

---
+++ LA1_SAHAGUN_CS1A/Boolean.js
@@ -0,0 +1,8 @@
+//let the Variables
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 30;
+let add = favNumb + undefined;
+
+// consoling the output
+console.log(isJsProgrammingLanguage + "\n" + isJsHard + "\n" + favNumb + "\n" + add)
